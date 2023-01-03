071f505d9b0c0a2d943f20eaedd2e532d78b8f62
Andres D. Molins <amolinsdiaz@yahoo.es>
2023-01-03 17:44:15 +0100

    Change copy to execution time instead build time.

--- publish-runtime/entrypoint.sh
+++ publish-runtime/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+ls -lah
+
+# COPY runtime file
+cp -vap /github/workspace/$INPUT_RUNTIME_FILENAME /app/runtime/$INPUT_RUNTIME_FILENAME
+
 python3 /app/runtime/publish.py $INPUT_RUNTIME_FILENAME $INPUT_PRIVATE_KEY
 
 
